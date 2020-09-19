4b7d40d81d01eef77e92af829960ae191982fedc
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-09-20 00:39:21 +0300

    first functions

--- src/__tests__/firstFunc.test.ts
+++ src/__tests__/firstFunc.test.ts
@@ -1,7 +1,16 @@
-import sum from "../firstFunc.js";
- 
+function sum(a, b) {
+    return a + b;
+}
+function biggerThenHalf() {
+    const result = Math.random() >= 0.5 ? "Yes" : "No";
+    return result;
+  }
 describe("First test", () => {
     test("Sum", () => {
-      expect(2 + 2).toEqual(4);
-    })
+      expect(sum(2, 7)).toEqual(9);
+    });
+
+    test("Random number bigger 0.5", () => {
+        expect(biggerThenHalf()).toEqual("Yes");
+    });
 })
--- src/random.js
+++
@@ -1,5 +0,0 @@
-// const num = Math.random();
-// if (num <= 0,5) {
-//     console.log("Yes")
-// }
-const num = Math.random() >= 0.5 ? console.log("Yes") : console.log("No");
